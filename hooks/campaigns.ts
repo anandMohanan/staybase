e520ef78f005b67fcb722a93700a3284e2c4ccd6
anandMohanan <ananthkvmohanan@gmail.com>
2025-01-08 00:42:37 +0530

    campaign edit and delete setup

--- hooks/campaigns.ts
+++ hooks/campaigns.ts
@@ -1,4 +1,4 @@
-import { MinimalCampaignSchema } from "@/lib/types/campaign";
+import { campaignFormSchema, MinimalCampaignSchema } from "@/lib/types/campaign";
 import { useQuery } from "@tanstack/react-query";
 
 export const useGetCampaigns = () => {
@@ -10,6 +10,30 @@ export const useGetCampaigns = () => {
 			console.log(data, "campaigns");
 			const validatedData = MinimalCampaignSchema.safeParse(data);
 			return validatedData.success ? validatedData.data : [];
+		},
+		enabled: true,
+		refetchOnWindowFocus: false,
+		refetchOnMount: false,
+	});
+};
+
+export const useGetCampaignById = (campaignId: string) => {
+	return useQuery({
+		queryKey: ["campaign", campaignId],
+		queryFn: async () => {
+			const res = await fetch("/api/campaign/get-campaign-by-id", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					campaignId: campaignId,
+				}),
+			});
+			const data = await res.json();
+			console.log(data, "campaign");
+			const validatedData = campaignFormSchema.safeParse(data);
+			return validatedData.success ? validatedData.data : null;
 		},
 		enabled: true,
 		refetchOnWindowFocus: false,
--- server/campaign.ts
+++ server/campaign.ts
@@ -4,14 +4,15 @@ import { db } from "@/db";
 import { CAMPAIGNS_TABLE } from "@/db/schema/campaign";
 import { auth } from "@/lib/auth";
 import type { CampaignFormValues } from "@/lib/types/campaign";
+import { eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
 import { v4 as uuidv4 } from "uuid";
 
 export const createCampaign = async (campaignData: CampaignFormValues) => {
 	try {
-        console.log(campaignData)
-        console.log(campaignData.startDate)
+		console.log(campaignData);
+		console.log(campaignData.startDate);
 		const organization = await auth.api.listOrganizations({
 			headers: await headers(),
 		});
@@ -29,7 +30,7 @@ export const createCampaign = async (campaignData: CampaignFormValues) => {
 			updatedAt: new Date(),
 			createdAt: new Date(),
 		});
-        revalidatePath('/dashboard/campaigns/active')
+		revalidatePath("/dashboard/campaigns/active");
 		return {
 			success: true,
 			message: "Campaign created successfully",
@@ -40,6 +41,48 @@ export const createCampaign = async (campaignData: CampaignFormValues) => {
 			success: false,
 			message:
 				error instanceof Error ? error.message : "Error creating campaign",
+			error: error instanceof Error ? error.toString() : "Unknown error",
+		};
+	}
+};
+
+export const deleteCampaign = async (campaignId: string) => {
+	try {
+		await db.delete(CAMPAIGNS_TABLE).where(eq(CAMPAIGNS_TABLE.id, campaignId));
+		return {
+			success: true,
+			message: "Campaign deleted successfully",
+		};
+	} catch (error) {
+		console.error("Error deleting campaign:", error);
+		return {
+			success: false,
+			message:
+				error instanceof Error ? error.message : "Error deleting campaign",
+			error: error instanceof Error ? error.toString() : "Unknown error",
+		};
+	}
+};
+
+export const updateCampaign = async (
+	campaignId: string,
+	campaignData: CampaignFormValues,
+) => {
+	try {
+		await db
+			.update(CAMPAIGNS_TABLE)
+			.set(campaignData)
+			.where(eq(CAMPAIGNS_TABLE.id, campaignId));
+		return {
+			success: true,
+			message: "Campaign updated successfully",
+		};
+	} catch (error) {
+		console.error("Error updating campaign:", error);
+		return {
+			success: false,
+			message:
+				error instanceof Error ? error.message : "Error updating campaign",
 			error: error instanceof Error ? error.toString() : "Unknown error",
 		};
 	}
